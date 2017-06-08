8e462d9731e40f97347e86eceb46abe57e0efbcb
Anima Dei <animadei@mail.ru>
2017-06-08 18:07:37 +1000

    en-ru => ru-en

--- 2.js
+++ 2.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 
 rl.question('Введите слово, которое нужно перевести на английский:', function(text){
     const key = 'trnsl.1.1.20170608T062213Z.9c730839ad5dc73a.d80e9047672c3868dff629b47c3c32c6dd49ca93';
-    const url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + key + '&text=' + text + '&lang=en-ru';
+    const url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + key + '&text=' + text + '&lang=ru-en';
     console.log(url);
     https.get(url, function(res){
         console.log(res); //распарсить не знаю как
